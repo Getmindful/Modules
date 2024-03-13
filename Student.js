bdf6d9b66110fbbacc0491c280e872a7e9c7b447
Abdullah <abdullahboy241@gmail.com>
2024-03-14 03:54:02 +0500

    first commit

---
+++ Student.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class login {
+    data = "login done";
+}
+exports.default = login;
---
+++ app.ts
@@ -0,0 +1,7 @@
+import slogin from "./Student";
+import tlogin from "./Teacher";
+let teacher= new tlogin();
+console.log(teacher.data)
+
+let student= new slogin();
+console.log(student.data)
